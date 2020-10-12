2fea3b25540b16ad7574aad59d8ea88bb68a9a2e
priyeshdabre <priyesh.chrisel@gmail.com>
2020-10-12 12:21:11 +0530

    Updated Ui

---
+++ .dart_tool/flutter_build/9a63cad503624a1ee1beb5be00928150/main.dart
@@ -0,0 +1,10 @@
+import 'dart:ui' as ui;
+
+import 'package:my_portfolio_app/main.dart' as entrypoint;
+
+Future<void> main() async {
+  if (true) {
+    await ui.webOnlyInitializePlatform();
+  }
+  entrypoint.main();
+}
